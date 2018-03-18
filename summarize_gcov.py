fe7b81e78b199f23a4b903c63a6e8eb684de76ec
Gabor Horvath <xazax.hun@gmail.com>
2018-03-18 12:32:05 +0100

    Prevent division by zero error.

--- summarize_gcov.py
+++ summarize_gcov.py
@@ -34,18 +34,20 @@ def summarize_gcov(path):
                     file_sum += value
                     if value > file_max:
                         file_max = value
-            summary[file_path] = {"max": file_max, "coverage": file_covered / (file_covered + file_missed),
+            file_all = max(file_covered + file_missed, 1)
+            summary[file_path] = {"max": file_max, "coverage": file_covered / file_all,
                                   "sum": file_sum, "missed": file_missed, "covered": file_covered,
-                                  "average": file_sum / (file_covered + file_missed), "noop": file_noop}
+                                  "average": file_sum / file_all, "noop": file_noop}
             if file_max > overall_max:
                 overall_max = file_max
             overall_covered += file_covered
             overall_missed += file_missed
             overall_noop += file_noop
             overall_sum += file_sum
-    summary["overall"] = {"max": overall_max, "coverage": overall_covered / (overall_covered + overall_missed),
+    overall_all = max(overall_covered + overall_missed, 1)
+    summary["overall"] = {"max": overall_max, "coverage": overall_covered / overall_all,
                           "sum": overall_sum, "missed": overall_missed, "covered": overall_covered,
-                          "average": overall_sum / (overall_covered + overall_missed), "noop": overall_noop}
+                          "average": overall_sum / overall_all, "noop": overall_noop}
     return summary
 
 
