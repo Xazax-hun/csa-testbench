8f183643c40d9af4c509866342ace63d43b16cf5
TrellixVulnTeam <112716341+TrellixVulnTeam@users.noreply.github.com>
2022-11-21 12:28:39 -0600

    Adding tarfile member sanitization to extractall() (#54)

--- run_experiments.py
+++ run_experiments.py
@@ -97,7 +97,26 @@ def clone_project(project: dict, project_dir: str, source_dir: str,
                                 ".tbz", ".tlz", ".txz")):
         path, _ = urlretrieve(project['url'])
         with tarfile.open(path) as tar:
-            tar.extractall(project_dir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, project_dir)
         content = os.listdir(project_dir)
         # If the tar contains a single directory, move contents up.
         if len(content) == 1:
